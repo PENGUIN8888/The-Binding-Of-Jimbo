38f99789ff966c609876d4e32983f26687f9cf7a
PENGUIN8888 <157919772+PENGUIN8888@users.noreply.github.com>
2025-01-11 15:29:46 -0600

    Update decks.lua

--- Items/decks.lua
+++ Items/decks.lua
@@ -59,6 +59,33 @@ local LostDeck = {
 		}))
     end
 }
+local AzazelDeck = {
+    object_type = "Back",
+    name = "wrenbind-Isaac",
+    key = "isaac",
+    loc_txt = {
+        name = "Isaac Deck",
+        text = {
+            "Start with an",
+            "{C:attention}Eternal Negative{}",
+            "Brimstone Joker."
+        }
+    },
+    order = 1,
+    pos = { x = 0, y = 0 },
+    atlas = "atlasdeck",
+    apply = function()
+        G.E_MANAGER:add_event(Event({
+			func = function()
+                WrenBind.util.deck_joker({
+                    joker = "wrenbind_Brimstone",
+                    negative = true
+                })
+				return true
+			end
+		}))
+    end
+}
 return {
     name = "Character Decks",
     items = {
