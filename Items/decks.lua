6565220e84af164f6d98b428fbbd6f8f95e0a9b1
PENGUIN8888 <157919772+PENGUIN8888@users.noreply.github.com>
2025-01-11 14:33:45 -0600

    Update decks.lua
    Step 1 of deck

--- Items/decks.lua
+++ Items/decks.lua
@@ -25,11 +25,39 @@ local IsaacDeck = {
 		}))
     end
 }
-
+local LostDeck = {
+    object_type = "Back",
+    name = "wrenbind-Lost",
+    key = "thelost",
+    loc_txt = {
+        name = "Lost Deck",
+        text = {
+            "Start with an",
+            "{C:attention}Eternal Negative{}",
+            "Holy Mantle Joker.",
+            "Only 1 Hand and Discard."
+        }
+    },
+    order = 1,
+    pos = { x = 0, y = 0 },
+    atlas = "atlasdeck",
+    apply = function()
+        G.E_MANAGER:add_event(Event({
+			func = function()
+                WrenBind.util.deck_joker({
+                    joker = "",
+                    negative = true
+                })
+				return true
+			end
+		}))
+    end
+}
 return {
     name = "Character Decks",
     items = {
         IsaacDeck
+	LostDeck
     },
     
 }
